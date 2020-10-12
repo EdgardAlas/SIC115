bdfbfbe216dfa06631f1e5a447dc2504a560831d
EdgardAlas <edgardalasu@gmail.com>
2020-10-11 18:38:39 -0600

    📚 text wrap pdf libro diario

--- app/views/pdf/libro-diario.php
+++ app/views/pdf/libro-diario.php
@@ -8,9 +8,12 @@ use Fpdf\Fpdf;
 class PDF extends FPDF
 {
 
+    public $widths;
+    public $aligns;
     public $empresa;
 
-    public function __construct($empresa) {
+    public function __construct($empresa)
+    {
         parent::__construct();
         $this->empresa = $empresa;
     }
@@ -44,6 +47,112 @@ class PDF extends FPDF
         $this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . ' / {nb}', 0, 0, 'C');
     }
 
+    public function SetWidths($w)
+    {
+        //Set the array of column widths
+        $this->widths = $w;
+    }
+
+    public function SetAligns($a)
+    {
+        //Set the array of column alignments
+        $this->aligns = $a;
+    }
+
+    public function Row($data)
+    {
+        //Calculate the height of the row
+        $nb = 0;
+        for ($i = 0; $i < count($data); $i++) {
+            $nb = max($nb, $this->NbLines($this->widths[$i], $data[$i]));
+        }
+
+        $h = 5 * $nb;
+        //Issue a page break first if needed
+        $this->CheckPageBreak($h);
+        //Draw the cells of the row
+        for ($i = 0; $i < count($data); $i++) {
+            $w = $this->widths[$i];
+            $a = isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+            //Save the current position
+            $x = $this->GetX();
+            $y = $this->GetY();
+            //Draw the border
+            $this->Rect($x, $y, $w, $h);
+            //Print the text
+            $this->MultiCell($w, 5, $data[$i], 0, $a);
+            //Put the position to the right of the cell
+            $this->SetXY($x + $w, $y);
+        }
+        //Go to the next line
+        $this->Ln($h);
+    }
+
+    public function CheckPageBreak($h)
+    {
+        //If the height h would cause an overflow, add a new page immediately
+        if ($this->GetY() + $h > $this->PageBreakTrigger) {
+            $this->AddPage($this->CurOrientation);
+        }
+
+    }
+
+    public function NbLines($w, $txt)
+    {
+        //Computes the number of lines a MultiCell of width w will take
+        $cw = &$this->CurrentFont['cw'];
+        if ($w == 0) {
+            $w = $this->w - $this->rMargin - $this->x;
+        }
+
+        $wmax = ($w - 2 * $this->cMargin) * 1000 / $this->FontSize;
+        $s = str_replace("\r", '', $txt);
+        $nb = strlen($s);
+        if ($nb > 0 and $s[$nb - 1] == "\n") {
+            $nb--;
+        }
+
+        $sep = -1;
+        $i = 0;
+        $j = 0;
+        $l = 0;
+        $nl = 1;
+        while ($i < $nb) {
+            $c = $s[$i];
+            if ($c == "\n") {
+                $i++;
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+                continue;
+            }
+            if ($c == ' ') {
+                $sep = $i;
+            }
+
+            $l += $cw[$c];
+            if ($l > $wmax) {
+                if ($sep == -1) {
+                    if ($i == $j) {
+                        $i++;
+                    }
+
+                } else {
+                    $i = $sep + 1;
+                }
+
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+            } else {
+                $i++;
+            }
+
+        }
+        return $nl;
+    }
 }
 
 $pdf = new PDF($empresa);
@@ -56,24 +165,29 @@ $pdf->SetTextColor(0);
 $pdf->SetFont('');
 
 $contador_detalle = 0;
-$cantidad_detalle= 1;
+$cantidad_detalle = 1;
+
+$pdf->SetWidths(array(25,100,32.5,32.5));
+$pdf->SetAligns(array('L', 'L', 'R', 'R'));
 
 foreach ($data as $key => $partida) {
 
-    if($contador_detalle===0){
+    if ($contador_detalle === 0) {
         $cantidad_detalle = array_count_values(array_column($data, 'numero'))[$partida['numero']];
     }
+    $pdf->Row(array(
+        $contador_detalle == 0 ? Utiles::fechaSinFormato($partida['fecha']) : '',
+        $partida['movimiento'] === 'Cargo' ? $partida['cuenta'] : '    ' . $partida['cuenta'],
+        ($partida['movimiento'] === 'Cargo' ? Utiles::monto($partida['monto']) : ''),
+        ($partida['movimiento'] === 'Abono' ? Utiles::monto($partida['monto']) : '')
+    ));
+    
 
-    $pdf->Cell(25, 5, $contador_detalle==0 ? Utiles::fechaSinFormato($partida['fecha']) : '', 'LRB', 0, 'L');
-    $pdf->Cell(100, 5, $partida['movimiento']==='Cargo' ? $partida['cuenta'] : '    '.$partida['cuenta'], 'LRB', 0, 'L');
-    $pdf->Cell(32.5, 5, ($partida['movimiento'] === 'Cargo' ? Utiles::monto($partida['monto']) : ''), 'LRB', 0, 'R');
-    $pdf->Cell(32.5, 5, ($partida['movimiento'] === 'Abono' ? Utiles::monto($partida['monto']) : ''), 'LRB', 1, 'R');
-
-    if($contador_detalle<$cantidad_detalle){
+    if ($contador_detalle < $cantidad_detalle) {
         $contador_detalle++;
     }
 
-    if($contador_detalle===$cantidad_detalle){
+    if ($contador_detalle === $cantidad_detalle) {
         $pdf->SetFont('courier', 'B', 9);
         $pdf->Cell(25, 5, '', 'LRB', 0, 'L');
         $pdf->Cell(100, 5, $partida['descripcion'], 'LRB', 0, 'C');
@@ -88,10 +202,7 @@ foreach ($data as $key => $partida) {
         $pdf->SetFont('courier', '', 9);
     }
 }
-$pdf->Cell(70, 6, '', 'T');
-$pdf->Cell(40, 6, '', 'T');
-$pdf->Cell(40, 6, '', 'T');
-$pdf->Cell(40, 6, '', 'T');
+
 $pdf->Ln();
 
 /* $path = "temp/".$id;
